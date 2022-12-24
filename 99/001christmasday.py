e7fde304b29dd46153e3b630bd0ed441c1735f15
prex-aliya <droved-through@proton.me>
2022-12-24 15:54:01 +0000

    added to 99

--- 99/001christmasday.py
+++ 99/001christmasday.py
@@ -4,16 +4,20 @@
 
 
 from datetime import datetime;
+import time;
 import sys;
 
 
 prompt_user = False;
-user_date = False;
-day = 26;
-month = 12;
-year = 2020;
-arguments = sys.argv;
-length = len(arguments);
+user_date   = False;
+# family tradition christmas starts at 8
+hour_c  = 8;
+day     = 26;
+month   = 12;
+year    = 2020;
+count_intraval  = 0.1;
+arguments   = sys.argv;
+length      = len(arguments);
 
 
 def verbose_true(input):
@@ -48,15 +52,77 @@ def user_input():
                 error("Possible input violation");
 
 
+def find_current():
+    current = datetime.now();
+    return current;
+def find_delta():
+    current = datetime.now();
+    year_c = current.year;
+
+    if month == 12 and day <= 25 and hour <= 8:
+        year_c += 1;
+
+    try: date2 = datetime(day=25, month=12, year=int(year_c),
+                         hour=int(hour_c));
+    except: error("failed to convert date into intager");
+    return date2;
+
+def print_left(timedelta):
+    try: print("Only {0} Day(s) and {1} Hour(s) Left!".format(timedelta.days,
+                    round(timedelta.seconds/60/60, 2)));
+    except: error("Failed to print time left");
+
+def count_down():
+    current = find_current();
+    date2 = find_delta();
+    countdelta = date2 - current;
+
+    if verbose == True:
+        countprint = "[*] Seconds left: ";
+    else: countprint = "";
+
+    verbose("Count Down To Crhistmas Started");
+    while (current.second - countdelta.seconds) != 0:
+        current = datetime.now();
+        countdelta = date2 - current;
+
+        #if countdelta.days < 1:
+        print("{0}".format(countdelta));
+        print("{0}{1}".format(countprint, countdelta.seconds));
+
+        time.sleep(0.1);
+        print("\033[3A");
+
+    print_left(countdelta);
+    verbose("Count Down To Christmas Started");
+    sys.exit()
+
+
 to_verbose = False;
 if length != 1:
     for x in range(1, length):
         if arguments[x] == "-v":
             to_verbose = True;
-            positive("verbose enabled");
-        if arguments[x] == ("-help" or "--help"):
+            positive("Verbose Enabled");
+        elif arguments[x] in ("-c",  "--count-down"):
+            count_down();
+        elif arguments[x] in ("-h", "--hour"):
+            try:
+                hour_c = int(arguments[x+1]);
+                verbose("User specified hour");
+            except:
+                warning("Hour input violation defaulting to 8");
+                hour_c = 8;
+        elif arguments[x] in ("-i", "--interval"):
+            try:
+                count_intraval = int(arguments[x+1]);
+                verbose("User Specified Count Down Interval");
+            except:
+                warning("Input violation defaulting to 1");
+                hour_c = 1;
+        elif arguments[x] in ("-help", "--help"):
             help();
-        if arguments[x] == "-d":
+        elif arguments[x] == "-d":
             positive("cmd arguments enabled");
             break;
 
@@ -67,25 +133,20 @@ if length != 1:
 
 
 def main():
-    if prompt_user == True:
-        current = datetime(day=int(day), month=int(month), year=int(year));
-        year_c = datetime.now().strftime("%Y");
-        verbose("Using user specified date");
+    verbose_true("");
+
+    if prompt_user == True: error("Prompt User is not implemented");
     else:
         try:
             current = datetime.now();
-            year_c = datetime.now().strftime("%Y");
-        except: warning("Failed to Get Current Time\n\twill use defult time");
+            year_c = int(current.year);
+        except:
+            error("Failed to use default time"
+                        .format(year_c));
 
-    if current.month == 12 and current.day >= 25:
-        try: year_c = int(year_c);
-        except: error("Failed to convert year into intager");
-    try: date2 = datetime(day=25, month=12, year=int(year_c),
-                          hour=8);
-    # personal family tradition
-    except: error("Failed to convert date into intager");
+    verbose("Current Year {}".format(year_c));
 
-
+    date2 = find_delta();
     timedelta = date2 - current;
 
     delta_seconds = timedelta.seconds;
@@ -99,8 +160,7 @@ def main():
     verbose("Seconds: {}".format(timedelta.seconds));
     verbose("MicroSeconds: {}".format(timedelta.microseconds));
 
-    print("Only {0} Days and {1} Hours Left!".format(timedelta.days,
-                    round(timedelta.seconds/60/60, 2)));
+    print_left(timedelta);
 
 
 
